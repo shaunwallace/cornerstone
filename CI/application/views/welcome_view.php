16408eed5c213a072825ab1a7cf9bc6a6301058e
shaun wallace <swallace6@gatech.edu>
2011-10-06 15:52:36 -0400

    changes to css and footer styles

--- CI/application/views/welcome_view.php
+++ CI/application/views/welcome_view.php
@@ -53,7 +53,8 @@
 </head>
 <body>
 
-<div class="wrapper">
+<div id="wrapper">
+	<div id="inner_wrapper">
 	<header>
 			<hgroup>
 				<h1><?php echo anchor('welcome/', 'Cornerstone Kitchen and Bath'); ?></h1>
@@ -101,14 +102,23 @@
 			</form>	
 		</aside>
 	</div><!-- end content -->
-	<footer>
-		<nav>
-		</nav><!-- end nav -->
+</div><!-- end inner_wrapper -->
+</div><!--end wrapper -->
+<footer>
+	<nav id="footer_nav">
+		<ul>
+			<li><?php echo anchor('welcome/', 'Home', 'title="home"'); ?></li>
+			<li><?php echo anchor('about/', 'About Us', 'title="about us"'); ?></li>
+			<li><?php echo anchor('products/', 'Products', 'title="products"'); ?></li>
+			<li><?php echo anchor('gallery/', 'Gallery', 'title="gallery"'); ?></li>
+			<li><?php echo anchor('contact/', 'Contact', 'title="contact"'); ?></li>
+		</ul>
+	</nav><!-- end nav -->
+	<div id="footer_wrapper">
 		<p id="copyright">&copy;2011 Cornerstone Kitchens and Baths. All rights reserved.</p>
 		<p id="address">840 Ernest W. Barrett Pkwy, Ste 584 Kennesaw, GA 30144 :: Tel 770-422-5077</p>
-		<!--<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>-->
-	</footer><!-- end footer -->
-</div><!--end wrapper -->
-
+	</div><!-- end footer_wrapper -->
+	<!--<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>-->
+</footer><!-- end footer -->
 </body>
 </html>
