1bd9f85fa30d4d6b956a1d207dcb122f583a6289
shaun wallace <swallace6@gatech.edu>
2011-10-06 16:11:46 -0400

    changes logo font and adjusted styles to finalize header and footer

--- CI/application/views/welcome_view.php
+++ CI/application/views/welcome_view.php
@@ -8,7 +8,8 @@
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.4.1/build/cssreset/cssreset-min.css">
 	<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:700'>
 	<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Coustard'>
-	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox-1.3.4.css"  media="screen" />
+	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox-1.3.4.css"  media="screen">
+	<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Smythe'>
 	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
