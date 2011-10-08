2e0405a125a82e51168c069cb0a55053a322d3d9
shaun wallace <swallace6@gatech.edu>
2011-10-08 16:26:09 -0400

    minor changes to stylesheets, allow for more content to be seen above the fold

--- CI/application/views/content_home.php
+++ CI/application/views/content_home.php
@@ -4,14 +4,14 @@
 <div id="content">
 	<div id="content_text_wrapper">
 		<h2>Who We Are</h2>
-		<p>Cornerstone is a Company name used by the Ellis Family for over thirty years. Most of those years were spent in the Home building Industry. In 2004 we turned our attention to the cabinet Industry. Cornerstone Kitchens and Baths is our latest creation were we have developed a unique Showroom in Kennesaw Ga. to sell Quality Cabinets to contractors and the public. 
+		<p>Cornerstone is a Company name used by the Ellis Family for over thirty years. Most of those years were spent in the Home building Industry. In 2004 we turned our attention to the cabinet Industry. Cornerstone Kitchens and Baths is our latest creation where we have developed a unique showroom in Kennesaw Ga. to sell Quality Cabinets to contractors and the public. 
 		</p>
 		
 		<a id="single_image" href="images/charleston_black1_large.jpg"><img src="images/charleston_black1.jpg" class="thumbnail" alt='Some information on this style of cabinet'></a>
 		<a id="single_image" href="images/newbury_large.jpg"><img src="images/newbury.jpg" class="thumbnail"></a>
 
 		<h2>What We Do</h2>
-		<p>Our business was created based on a product line that is a Solid wood Cabinet with features not usually seen as standard. We are also able to accommodate any special needs our customers may have. We can call on several manufacturers and even create custom finishes on many of our products. Visit our showroom and let us save you money on your new Kitchen.</p>
+		<p>Our business was created based on a product line that is a solid wood cabinet with features not usually seen as standard. We are also able to accommodate any special needs our customers may have. We can call on several manufacturers and even create custom finishes on many of our products. Visit our showroom and let us save you money on your new kitchen.</p>
 	</div><!-- end content_text_wrapper -->
 	<aside>
 		<h2>Want A Quote?</h2>
