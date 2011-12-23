43761da56b65ebc109ec6f0acda2adf9c687ab81
Jeff Kumar <Jeff.s.kumar@gmail.com>
2011-12-22 21:19:48 -0500

    added an about us page

---
+++ CI/application/views/about.php
@@ -0,0 +1,15 @@
+ <div class="banner">
+    <?php echo img('images/banner_image_2.jpg'); ?>
+</div><!-- end banner -->
+<div id="content">
+<h1>We will beat any price...for any product.</h1>
+    <div id="about_us_text" class="cabinet_styles"> 
+    <h2>About Cornerstone</h2>                                                        
+      <p>Cornerstone is a Company name used by the Ellis Family for over thirty years.  Most of those years were spent in the Home building Industry.  In 2004 we turned our attention to the cabinet Industry.  Cornerstone Kitchens and Baths is our latest creation were we have developed a unique Showroom in Kennesaw Ga. to sell Quality Cabinets to contractors and the public.  With our background in Construction, and cabinet design we feel like we can bring a unique prospective to our customers.  So whether you need insight on a total Kitchen and Bath transformation or simply a Vanity in a guest bathroom we are a team equipped to make your dreams a reality.</p>
+      <p> Our business was created based on a product line that is a Solid wood Cabinet with features not usually seen as standard.  We are also able to accommodate any special needs our customers may have.  We can call on several manufacturers and even create custom finishes on many of our products.  Visit our showroom and let us save you money on your new Kitchen. <br /></p>                                  
+      <br />
+      <br />
+    </div><!-- /about us text -->                                   
+</div><!--  /content -->
+ 
+                                                                                                                                                                                                                                                                                                                 
