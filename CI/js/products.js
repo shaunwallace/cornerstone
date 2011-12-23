3db61c820ff9eaf892e43fe1029602f67e606557
shaun wallace <swallace6@gatech.edu>
2011-12-22 20:06:07 -0500

    minor style changes

--- CI/js/products.js
+++ CI/js/products.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
+
+/*
+
+function toggleText( showLink, hideLink, hideTextID ) {
+	$( showLink ).click(function() {
+		$( showLink ).fadeToggle( 'fast', 'swing', function() {
+			$( hideTextID ).fadeToggle( 'slow', 'swing');
+		});
+	});
+	
+	$( hideLink ).click(function() {
+		$( hideTextID ).fadeToggle( 'slow', 'swing', function() {
+			$( showLink ).fadeToggle( 'slow', 'swing');
+		});
+	});
+}
+
+*/
+
 	$('.read_more_links').click(function() {
-	  	$('.read_more_links').toggle('fast', function() {
-  			$('#charleston_more_text').slideToggle('slow', 'swing');	  	
-	  	});
+		$('.read_more_links').fadeToggle('fast', 'swing', function() {
+			$('#charleston_more_text').fadeToggle('slow', 'swing');
+		});
 	});
 	
 	$('.read_less_links').click(function() {
-	  	
-  		$('#charleston_more_text').fadeToggle('fast', 'swing', function() {
-  			$('.read_more_links').toggle('slow', 'linear');
+  		$('#charleston_more_text').fadeToggle('slow', 'swing', function() {
+  			$('.read_more_links').fadeToggle('slow', 'swing');
   		});
 	});
+
 });
